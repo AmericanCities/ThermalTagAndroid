782affc2c549a60b6ff82d2500a4b36105af2256
Matt Heering <matt.heering@gmail.com>
2016-04-19 02:46:09 -0400

    Added to OysterScan class and changes to Camera Activity

--- app/src/main/java/thermaltag/thermaltag/CameraActivity.java
+++ app/src/main/java/thermaltag/thermaltag/CameraActivity.java
@@ -8,12 +8,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -26,6 +28,9 @@ public class CameraActivity extends AppCompatActivity {
     private static int TAKE_PICTURE=1;
     private Uri imageUri;
     private String username;
+    private ImageButton cameraButton, submitButton;
+    private EditText shipper_cert, harvest_date, harvest_location, type_of_shellfish, quantity,
+    temperature, geo_location, time_of_scan, date_of_scan, scan_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +38,35 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ImageButton cameraButton =(ImageButton)findViewById(R.id.button_camera);
-        cameraButton.setOnClickListener(cameraListener);
+
+        // Get "extra" username passed from Login Activity
         username = getIntent().getExtras().getString("username");
 
+        // set view variables from layout
+        createViewVariables();
+
+        // set listeners
+      cameraButton.setOnClickListener(cameraListener);
+      submitButton.setOnClickListener(submitListner);
+    }
+
+    // set view variables
+    private void createViewVariables() {
+        //buttons
+        cameraButton        = (ImageButton)findViewById(R.id.button_camera);
+        submitButton        = (ImageButton)findViewById(R.id.button_submit);
+
+        //editText fields
+        shipper_cert        = (EditText)findViewById(R.id.origin_shipper_cert);
+        harvest_date        = (EditText)findViewById(R.id.harvest_date);
+        harvest_location    = (EditText)findViewById(R.id.harvest_location);
+        type_of_shellfish   = (EditText)findViewById(R.id.type_of_shellfish);
+        quantity            = (EditText)findViewById(R.id.quantity);
+        temperature         = (EditText)findViewById(R.id.temperature);
+        geo_location        = (EditText)findViewById(R.id.geo_location);
+        time_of_scan        = (EditText)findViewById(R.id.time_of_scan);
+        date_of_scan        = (EditText)findViewById(R.id.date_of_scan);
+        scan_id             = (EditText)findViewById(R.id.scan_id);
     }
 
     @Override
@@ -66,6 +96,27 @@ public class CameraActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private View.OnClickListener submitListner = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            // this could probably be refactored as an array of textViews
+            String entered_shipper_cert = shipper_cert.getText().toString();
+            String entered_harvest_date = harvest_date.getText().toString();
+            String entered_harvest_location = harvest_location.getText().toString();
+            String entered_type_of_shellfish = type_of_shellfish.getText().toString();
+            String entered_quantity = quantity.getText().toString();
+            String entered_temperature = temperature.getText().toString();
+            String entered_geo_location = geo_location.getText().toString();
+            String entered_time_of_scan = time_of_scan.getText().toString();
+            String entered_date_of_scan = date_of_scan.getText().toString();
+            // not sure if we need scan_id
+
+            if (entered_shipper_cert.equals("")) {
+                Snackbar.make(v, "Shipper Certificate ID must be filled out", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            } // end if
+        }
+    };
 
 
     private View.OnClickListener cameraListener = new View.OnClickListener() {
--- app/src/main/java/thermaltag/thermaltag/OysterScan.java
+++ app/src/main/java/thermaltag/thermaltag/OysterScan.java
@@ -5,4 +5,103 @@ public class OysterScan {
     public String oyster_type;
     public String quantity;
     public String status;
+    public String shipper_cert;
+    public String harvest_date;
+    public String harvest_location;
+    public String temperature;
+    public String geo_location;
+    public String time_of_scan;
+    public String date_of_scan;
+
+    public String getOyster_type() {
+        return oyster_type;
+    }
+
+    public void setOyster_type(String oyster_type) {
+        this.oyster_type = oyster_type;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getShipper_cert() {
+        return shipper_cert;
+    }
+
+    public void setShipper_cert(String shipper_cert) {
+        this.shipper_cert = shipper_cert;
+    }
+
+    public String getHarvest_date() {
+        return harvest_date;
+    }
+
+    public void setHarvest_date(String harvest_date) {
+        this.harvest_date = harvest_date;
+    }
+
+    public String getHarvest_location() {
+        return harvest_location;
+    }
+
+    public void setHarvest_location(String harvest_location) {
+        this.harvest_location = harvest_location;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getGeo_location() {
+        return geo_location;
+    }
+
+    public void setGeo_location(String geo_location) {
+        this.geo_location = geo_location;
+    }
+
+    public String getTime_of_scan() {
+        return time_of_scan;
+    }
+
+    public void setTime_of_scan(String time_of_scan) {
+        this.time_of_scan = time_of_scan;
+    }
+
+    public String getDate_of_scan() {
+        return date_of_scan;
+    }
+
+    public void setDate_of_scan(String date_of_scan) {
+        this.date_of_scan = date_of_scan;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+
 }
