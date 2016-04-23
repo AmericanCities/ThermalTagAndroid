22f3c44a70724e1f3c6d59ba385764d3af8cdad0
Matt Heering <matt.heering@gmail.com>
2016-04-23 03:37:58 -0400

    Added Geo, Date/Time method, textView swap for EditView

--- app/src/main/java/thermaltag/thermaltag/CameraActivity.java
+++ app/src/main/java/thermaltag/thermaltag/CameraActivity.java
@@ -5,15 +5,23 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -30,8 +38,10 @@ import android.widget.Toast;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements LocationListener {
 
     // Declarations
     public static final String LOGTAG = "ThermalTag";
@@ -40,9 +50,12 @@ public class CameraActivity extends AppCompatActivity {
     private String username;
     private ImageButton cameraButton, submitButton;
     private EditText shipper_cert, harvest_date, harvest_location, type_of_shellfish, quantity,
-            temperature, geo_location, time_of_scan, date_of_scan, scan_id;
-    public static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-
+            temperature, scan_id;
+    private TextView date_of_scan, time_of_scan, geo_location;
+    public static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+                                            "Oct", "Nov", "Dec"};
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,11 +75,95 @@ public class CameraActivity extends AppCompatActivity {
         // set click listeners
         setClickListners();
 
-        // set current date of scan
 
+        // check if Location services is turned on
+        checkGpsOn();
+
+        // set geo location
+        getGeoLocation();
     }
 
-    // set view variables
+    // remember to prompt this if GPS value is empty
+    private void checkGpsOn() {
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        final boolean enabled = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // check if enabled and if not send user to the Location settings with pop-up
+        if (!enabled) {
+            // pop up dialogue for location services - sets listeners to the Y/N buttons within the dialogue.
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which){
+                        case DialogInterface.BUTTON_POSITIVE:
+                            //Yes button clicked
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                            break;
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            //No button clicked
+                            break;
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(CameraActivity.this);
+            builder.setMessage("This App requires location Services.  Turn on?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+
+        }
+    }
+
+    private void getGeoLocation() {
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider
+        //http://developer.android.com/reference/android/location/Criteria.html
+        Criteria criteria = new Criteria();
+
+        //http://developer.android.com/reference/android/location/LocationManager.html#getBestProvider(android.location.Criteria, boolean)
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            Log.i(LOGTAG,"Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            geo_location.setText("Location not available");
+        }
+    }
+
+//    // change geo location to string
+//    public static String locationStringFromLocation(final Location location) {
+//        return Location.convert(location.getLatitude(), Location.FORMAT_DEGREES) + " " + Location.convert(location.getLongitude(), Location.FORMAT_DEGREES);
+//    }
+
+    // set current date of scan
+    //setDateAndTime();
+    private void setDateAndTime() {
+        final Calendar c = Calendar.getInstance(Locale.getDefault());
+        int year         = c.get(Calendar.YEAR);
+        int month        = c.get(Calendar.MONTH);
+        int day          = c.get(Calendar.DAY_OF_MONTH);
+        int hour         = c.get(Calendar.HOUR_OF_DAY);
+        int minute       = c.get(Calendar.MINUTE);
+        int second       = c.get(Calendar.SECOND);
+        String amPm ="AM";
+
+        if (hour > 12){
+            hour-=12;
+            amPm="PM";
+        }
+
+        date_of_scan.setText(MONTHS[month] + " " + day + " " + year);
+        time_of_scan.setText(hour + ":" + minute + ":" +second + " " + amPm);
+    }
+
+
+    // This method sets all of our view variables
     private void createViewVariables() {
         //buttons
         cameraButton = (ImageButton) findViewById(R.id.button_camera);
@@ -79,10 +176,12 @@ public class CameraActivity extends AppCompatActivity {
         type_of_shellfish = (EditText) findViewById(R.id.type_of_shellfish);
         quantity = (EditText) findViewById(R.id.quantity);
         temperature = (EditText) findViewById(R.id.temperature);
-        geo_location = (EditText) findViewById(R.id.geo_location);
-        time_of_scan = (EditText) findViewById(R.id.time_of_scan);
-        date_of_scan = (EditText) findViewById(R.id.date_of_scan);
         scan_id = (EditText) findViewById(R.id.scan_id);
+
+        //TextView Fields -- populated automatically by phone - doesn't need to be edited.
+        date_of_scan = (TextView) findViewById(R.id.date_of_scan);
+        time_of_scan = (TextView) findViewById(R.id.time_of_scan);
+        geo_location = (TextView) findViewById(R.id.geo_location);
     }
 
     private void setClickListners(){
@@ -97,8 +196,6 @@ public class CameraActivity extends AppCompatActivity {
                 public void onClick(View v) {
                    DatePickerFragment datePicker = new DatePickerFragment();
                    datePicker.show(getSupportFragmentManager(), "datePicker");
-//                    saveData.setVisibility(View.VISIBLE);
-
                 }
             });
         }
@@ -190,6 +287,52 @@ public class CameraActivity extends AppCompatActivity {
     }
 
 
+    // location Listener Interface (Required)
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String lat = location.convert(location.getLongitude(), location.FORMAT_DEGREES);
+        String lng= location.convert(location.getLatitude(), location.FORMAT_DEGREES);
+
+        Log.i(LOGTAG,"The location is: " + lng + ", " + lat);
+        String locationString = "LNG: " + lng + " LAT: " + lat;
+        geo_location.setText(String.valueOf(locationString));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
     /*
      DATE PICKER FRAGMENT
      ---------------------------------------------------------------
@@ -214,15 +357,14 @@ public class CameraActivity extends AppCompatActivity {
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {
-            Calendar c = Calendar.getInstance();
-            c.set(year, month, day);
+            EditText dateSelected = (EditText)getActivity().findViewById(R.id.harvest_date);
+            dateSelected.setText(MONTHS[month] + " " + day + " " + year);
+
+//            In case we have time to implement local SQL-LITE
+//            Calendar c = Calendar.getInstance();
+//            c.set(year, month, day);
 //            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 //            sqlFormattedDate = sdf.format(c.getTime());
-//            displayDate.setText(MONTHS[month] + " " + day);
-            EditText dateSelected = (EditText)getActivity().findViewById(R.id.harvest_date);
-//            dateSelected.setText(MONTHS[month] + " " + day);
-            dateSelected.setText(MONTHS[month] + " " + day + " " + year);
-            Log.i(LOGTAG, "got Here Boom");
         }
     }
 }
